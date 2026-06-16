7090124598522067315c332988c09a899eca3ad2
michal-hynek <hynek.michal@gmail.com>
2026-06-16 06:29:48 -0700

    Added CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use axum::{Router, routing::post};
+use clap::Parser;
 use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
 use tokio::net::TcpListener;
@@ -7,6 +8,7 @@ use tokio::net::TcpListener;
 use crate::api::LinkRepository;
 
 mod api;
+mod cli;
 
 fn init_db_pool(db_path: &str) -> Result<Pool<SqliteConnectionManager>> {
     let manager = SqliteConnectionManager::file(db_path);
@@ -34,8 +36,10 @@ fn run_db_migrations(connection: PooledConnection<SqliteConnectionManager>) -> R
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let db_path = "src/db/links.db";
-    let db_pool = init_db_pool(db_path)?;
+    let args = crate::cli::Args::parse();
+
+    let db_path = args.db_path;
+    let db_pool = init_db_pool(&db_path)?;
     run_db_migrations(db_pool.get()?)?;
 
     let repository = LinkRepository::new(db_pool.clone());
@@ -43,10 +47,10 @@ async fn main() -> Result<()> {
         .route("/links", post(crate::api::create_link))
         .with_state(repository);
 
-    let addr = "0.0.0.0:8000";
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let addr = args.server_address;
+    let listener = TcpListener::bind(&addr).await.unwrap();
 
-    println!("Listening on {addr}");
+    println!("Listening on {}", &addr);
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
